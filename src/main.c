22871e96b219ea96f76db38aa308922cea7a0074
Christopher Goncalves <cgoncal12@gmail.com>
2025-07-19 17:48:26 -0700

    Started doing some parry and enemy movement stuff

--- src/main.c
+++ src/main.c
@@ -1,8 +1,24 @@
 #include "raylib.h"
 #include "types.h"
 #include <assert.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
+
+typedef enum {
+  IDLE,
+  WALK,
+  JUMP,
+  ATTACK,
+  RUN,
+  EVADE,
+  FALLING,
+  DEATH,
+  MOVE_RIGHT,
+  MOVE_LEFT,
+  PARRY
+} State;
 
 typedef struct {
   float x;
@@ -13,30 +29,70 @@ typedef struct {
   float height;
   Color color;
   bool onGround;
+  State state;
+  int attack_cooldown;
+  int attack_timer;
+  bool hit_registered;
 } Entity;
 
 Entity player = {
-    .x = 0,
-    .y = 0,
-    .dx = 3,
+    .x = 30,
+    .y = 450 - 32,
+    .dx = 3.5,
     .dy = 3,
-    .width = 32,
+    .width = 22,
     .height = 32,
     .onGround = false,
     .color = WHITE,
+    .state = IDLE,
 };
 
+Entity enemy = {
+    .x = 800 - 32 - 62,
+    .y = 450 - 32,
+    .dx = 3,
+    .dy = 3,
+    .width = 22,
+    .height = 32,
+    .onGround = false,
+    .color = PURPLE,
+    .state = IDLE,
+    .attack_cooldown = 0,
+};
+
+int parry_timer = 0;
 int main(void) {
   InitWindow(800, 450, "Parry");
   SetTargetFPS(60);
   while (!WindowShouldClose()) {
+    if (parry_timer > 0) {
+      parry_timer--;
+    }
+
     // Input
     if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) {
-      player.x -= player.dx;
+      if (player.x > 0) {
+        player.x -= player.dx;
+      }
     }
 
     if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
-      player.x += player.dx;
+      if (player.x < GetScreenWidth() - player.width) {
+        player.x += player.dx;
+      }
+    }
+
+    if (IsKeyPressed(KEY_SPACE) && parry_timer == 0) {
+      parry_timer = 10;
+      player.state = PARRY;
+    } else if (parry_timer == 0) {
+      player.state = IDLE;
+    }
+
+    if (player.state == PARRY) {
+      player.color = BLUE;
+    } else {
+      player.color = WHITE;
     }
 
     // Update
@@ -44,8 +100,34 @@ int main(void) {
       player.y += 4.5f;
     }
 
-    if (player.y > GetScreenHeight() - player.y) {
+    if (player.y > 450 - player.height) {
       player.onGround = true;
+    }
+
+    int distance = fabsf(enemy.x - player.x);
+    if (distance >= 0 && distance <= 50) {
+      enemy.state = ATTACK;
+    } else if (distance > 100) {
+      enemy.state = WALK;
+    } else {
+      enemy.state = IDLE;
+    }
+
+    switch (enemy.state) {
+    case ATTACK: {
+      enemy.color = YELLOW;
+      break;
+    }
+    case WALK: {
+      if (player.x > enemy.x) {
+        enemy.x += enemy.dx;
+      } else {
+        enemy.x -= enemy.dx;
+      }
+      break;
+    }
+    default:
+      enemy.color = PURPLE;
     }
 
     // Drawing
@@ -53,6 +135,12 @@ int main(void) {
     ClearBackground(BLACK);
     DrawRectangle(player.x, player.y, player.width, player.height,
                   player.color);
+
+    DrawRectangle(enemy.x, enemy.y, enemy.width, enemy.height, enemy.color);
+    if (enemy.state == ATTACK) {
+      DrawRectangle(enemy.x - enemy.width, enemy.y + (enemy.height / 2), 100, 5,
+                    enemy.color);
+    }
 
     EndDrawing();
   }
